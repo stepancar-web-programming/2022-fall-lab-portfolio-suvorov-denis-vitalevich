539497ec6acbb6a31b4d566923c64132344e7b77
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2022-12-11 02:26:04 +0300

    Upload project

---
+++ menu.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
