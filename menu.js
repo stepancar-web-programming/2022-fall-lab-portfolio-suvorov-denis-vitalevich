0f0935c74375156c6b1fa139be87e6f8edeecc0b
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2022-12-17 18:13:43 +0300

    Проверил с помощью eslint

--- menu.js
+++ menu.js
@@ -1,20 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav";
-    }
-}
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
-
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+// eslint-disable-next-line no-restricted-syntax
+for (const smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', (e) => {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
     });
-};
+  });
+}
