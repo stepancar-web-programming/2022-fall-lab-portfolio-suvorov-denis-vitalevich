445c916d68fb7409c91496426149f4bd4d29c376
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2022-12-25 22:01:15 +0300

    убрал n<1

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function showSlides(n) {
   const slides = document.getElementsByClassName('mySlides');
   if (n === undefined) { n = ++slideIndex; }
   if (n > slides.length) { slideIndex = 1; }
-  if (n < 1) { slideIndex = slides.length; }
   for (i = 0; i < slides.length; i++) { slides[i].style.display = 'none'; }
   slides[slideIndex - 1].style.display = 'block';
   setTimeout(showSlides, 5000);
