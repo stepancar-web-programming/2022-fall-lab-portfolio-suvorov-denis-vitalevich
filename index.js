b6b8f9ccf48530ebb2078f0e73229113c8dc1fe6
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2022-12-28 21:54:19 +0300

    изменил функцию

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ showSlides();
 function showSlides() {
   let i;
   const slides = document.getElementsByClassName('mySlides');
-  for (i = 0; i < slides.length; i++) {  slides[i].style.display = 'none'; }
   slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1; }
-  slides[slideIndex - 1].style.display = 'block';
+  for (i = 0; i < slides.length; i++) {  slides[i].style.display = 'none'; }
+  if (slideIndex >= slides.length) { slideIndex = 0; }
+  slides[slideIndex].style.display = 'block';
   setTimeout(showSlides, 5000);
 }
+
 
